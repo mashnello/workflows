0da80cdcbecd54b0dbf41deb95a1a3db05869de2
mashnello <mashnenko@mail.ru>
2016-05-15 19:24:23 +0300

    added coffeescript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('Workflows are awesome');
+var coffeeSourses = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+	gulp.src(coffeeSourses)
+		.pipe(coffee({ bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });

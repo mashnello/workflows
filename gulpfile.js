17e337c643c2fcc12d3c828a775749d932a4375d
mashnello <mashnenko@mail.ru>
2016-05-15 19:47:12 +0300

    added compass process and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,40 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var compass = require('gulp-compass');
 
 var coffeeSourses = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
+var sassSourses = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSourses)
 		.pipe(coffee({ bare: true})
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSourses)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
 });
